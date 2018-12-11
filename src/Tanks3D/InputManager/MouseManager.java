3b4a912587881401d4e5762c175d6f8d645e3e6f
AlexWolski <alex.theinfinite@gmail.com>
2018-12-11 01:32:10 -0800

    Made mouse invisible

--- src/Tanks3D/InputManager/MouseManager.java
+++ src/Tanks3D/InputManager/MouseManager.java
@@ -6,9 +6,12 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 //Handles mouse inputs.
 public class MouseManager extends MouseAdapter {
+    //The frame that the game is running in.
+    private final JFrame window;
     //The position of the frame on the screen.
     private final Point location;
     //The dimensions of the frame.
@@ -17,6 +20,11 @@ public class MouseManager extends MouseAdapter {
     private final PlayerController playerController;
     //A robot object used to move the mouse.
     private Robot robot;
+    //A cursor with no mouse icon.
+    private Cursor hiddenCursor;
+
+    //The sensitivity of the mouse.
+    private double sensitivity = 0.05;
 
     //Create a MouseManager object.
     public static void init(JFrame window, PlayerController playerController) {
@@ -25,6 +33,7 @@ public class MouseManager extends MouseAdapter {
 
     //MouseManager is a singleton class.
     private MouseManager(JFrame window, PlayerController playerController) {
+        this.window = window;
         this.playerController = playerController;
         location = window.getLocation();
         dimension = window.getSize();
@@ -36,9 +45,23 @@ public class MouseManager extends MouseAdapter {
             System.out.println(e.getMessage());
         }
 
+        //Create the hidden mouse.
+        BufferedImage dummy = new BufferedImage(1, 1, BufferedImage.TYPE_4BYTE_ABGR);
+        hiddenCursor = Toolkit.getDefaultToolkit().createCustomCursor(dummy, new Point(0, 0),"hidden cursor");
+
+        hideCursor();
+
         //Add this mouse manager to the window.
         window.addMouseListener(this);
         window.addMouseMotionListener(this);
+    }
+
+    private void hideCursor() {
+        window.setCursor(hiddenCursor);
+    }
+
+    private void resetCursor() {
+        window.setCursor(Cursor.getDefaultCursor());
     }
 
     @Override
@@ -53,13 +76,13 @@ public class MouseManager extends MouseAdapter {
 
     @Override
     public void mouseMoved(MouseEvent event) {
-        playerController.rotatePlayer((event.getX() - dimension.width/2.0) * 0.05);
+        playerController.rotatePlayer((event.getX() - dimension.width/2.0) * sensitivity);
         robot.mouseMove(location.x + dimension.width/2, location.y + dimension.height/2);
     }
 
     @Override
     public void mouseDragged(MouseEvent event) {
-        playerController.rotatePlayer((event.getX() - dimension.width/2.0) * 0.05);
+        playerController.rotatePlayer((event.getX() - dimension.width/2.0) * sensitivity);
         robot.mouseMove(location.x + dimension.width/2, location.y + dimension.height/2);
     }
 
